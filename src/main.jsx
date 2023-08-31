d79c9c8396382a3e76bfbdcfd8b869fc1a7976d7
TamaraFerr <tamarablacksmith@hotmail.com>
2023-08-31 18:59:19 -0300

    feat: layout SignUp almost ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import ResetStyle from './styles/ResetStyle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ResetStyle />
     <App />
   </React.StrictMode>,
 )
